5b8719b6f35b1c5303a4996f7b796eb8c3ccedcb
Jorge Saud <jsaud@modyo.com>
2020-06-25 12:28:14 -0400

    feat(test): add a sample test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,9 +1,13 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from '../../src/App.vue';
 
+const localVue = createLocalVue();
+localVue.component('font-awesome-icon', FontAwesomeIcon);
 describe('App.vue', () => {
   it('renders Basic info', () => {
     const wrapper = shallowMount(App, {
+      localVue,
     });
     expect(wrapper.text()).toContain('Build better digital products to unify your customer experiences and accelerate growth.');
   });
