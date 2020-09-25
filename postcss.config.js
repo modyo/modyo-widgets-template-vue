af71ff9a4d27d22571356e5a293c7585f688c885
Jorge Saud <jsaud@modyo.com>
2020-09-25 14:11:55 -0300

    fix: postcss config and manage autoprefixer version

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,26 @@
 const PURGE_CSS = require('@fullhuman/postcss-purgecss');
 
-const IN_PRODUCTION = process.env.NODE_ENV === 'production';
-const plugins = {};
-
-if (IN_PRODUCTION) {
-  plugins.purgecss = PURGE_CSS({
-    content: ['./public/**/*.html', './src/**/*.vue'],
-    defaultExtractor(content) {
-      const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
-      return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-    },
-    safelist: [
-      /-(leave|enter|appear)(|-(to|from|active))$/,
-      /^(?!(|.*?:)cursor-move).+-move$/,
-      /^router-link(|-exact)-active$/,
-      /data-v-.*/,
-      /svg.*/,
-      /fa.*/,
-      /^d-*/,
-    ],
-  });
-}
-
-plugins.autoprefixer = {};
+const AUTOPREFIXER = require('autoprefixer');
 
 module.exports = {
-  plugins,
+  plugins:
+    process.env.NODE_ENV === 'production'
+      // eslint-disable-next-line global-require
+      ? [AUTOPREFIXER, PURGE_CSS({
+        content: ['./public/**/*.html', './src/**/*.vue'],
+        defaultExtractor(content) {
+          const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+          return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+        },
+        safelist: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/,
+          /svg.*/,
+          /fa.*/,
+          /^d-*/,
+        ],
+      })]
+      : [AUTOPREFIXER],
 };
