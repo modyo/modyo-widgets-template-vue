ea1c776a8b59d339133c7f24987ad7c580dd6f6c
Juan Cristobal Pazos <juancri@modyo.com>
2021-03-10 10:42:49 -0300

    chore(dep): minor updates to dep

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import i18n from './i18n';
+import LiquidPlugin from './vue-liquid-plugin';
+import liquidConfig from './vue-liquid-config';
 import './vee-validate-config';
 import './vue-fontawesome-config';
 import './scss/custom.scss';
-import LiquidPlugin from './vue-liquid-plugin';
-import liquidConfig from './vue-liquid-config';
 
 if (process.env.NODE_ENV === 'development') {
   require('bootstrap');
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 Vue.config.productionTip = false;
+
 Vue.use(LiquidPlugin, liquidConfig);
 new Vue({
   store,
