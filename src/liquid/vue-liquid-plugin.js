65d8a16806488917cb60e91e75be0ea9e1ebe91a
Juan Cristobal Pazos <juancri@modyo.com>
2021-03-29 12:01:04 -0300

    refactor(liquid): use one definition file for liquidParser

--- src/liquid/vue-liquid-plugin.js
+++
@@ -1,8 +0,0 @@
-import LiquidParser from './liquidParser';
-
-export default {
-  install(Vue, options) {
-    // eslint-disable-next-line no-param-reassign
-    Vue.prototype.$liquid = new LiquidParser(options);
-  },
-};
