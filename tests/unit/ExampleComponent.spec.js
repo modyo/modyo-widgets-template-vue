a2f6ff43e49d3897690fe14009b3d8cb4841f365
Jorge Saud <jsaud@modyo.com>
2023-01-06 17:41:07 -0300

    fix: package & lock

--- tests/unit/ExampleComponent.spec.js
+++ tests/unit/ExampleComponent.spec.js
@@ -21,7 +21,7 @@ describe('ExampleComponent', () => {
     expect(wrapper.find('img').attributes().alt).toBe(imageAlt);
     expect(wrapper.find('h3').text()).toBe(title);
     expect(wrapper.find('p').text()).toBe(description);
-    expect(wrapper.find('.btn').attributes().href).toBe(`${wrapper.vm.siteUrl}/${wrapper.vm.contentViewPath}/${slug}`);
+    expect(wrapper.find('.btn').attributes().href).toBe(`${wrapper.vm.linkFull}`);
   });
 
   test('imageAlt has a default \'imagen\' value', () => {
