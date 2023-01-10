8a007e4452fb143d78f828bdb56dc8860966f871
Jorge Saud <jsaud@modyo.com>
2023-01-10 09:37:24 -0300

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports =  {
       node: true
     },
     extends: [
-      "plugin:vue/essential",
+      "plugin:vue/recommended",
       "eslint:recommended"
     ],
     globals:{
@@ -13,17 +13,25 @@ module.exports =  {
     parserOptions: {
       parser: "@babel/eslint-parser"
     },
-    rules: {},
+    rules: {
+      'max-len': [2, {
+        code: 120,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+        ignorePattern: 'd="([\\s\\S]*?)"',
+      }],
+      "vue/match-component-file-name": ["error", {
+        "extensions": ["jsx"],
+        "shouldMatchCase": false
+      }],
+      "vue/component-name-in-template-casing": ["error", "kebab-case", {
+        "registeredComponentsOnly": true,
+        "ignores": []
+      }]
+      
+    },
     overrides: [
-      {
-        files: [
-          '**/__tests__/*.{j,t}s?(x)',
-          '**/tests/unit/**/*.spec.{j,t}s?(x)'
-        ],
-        env: {
-          jest: true
-        }
-      },
       {
         files: [
           '**/__tests__/*.{j,t}s?(x)',
